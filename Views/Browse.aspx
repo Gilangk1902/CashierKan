f0e3483d63cbafd444aba460bb32af3b47b7eb26
HalizaSyafa27 <111560506+HalizaSyafa27@users.noreply.github.com>
2023-06-16 20:12:25 +0700

    Add files via upload

---
+++ Views/Browse.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Template.Master" AutoEventWireup="true" CodeBehind="Views.aspx.cs" Inherits="PSDProject.Views.Views" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Browse All Items</h1>
+
+    <!-- Search Form -->
+    <asp:TextBox ID="SearchTextBox" runat="server"></asp:TextBox>
+    <asp:Button ID="SearchButton" runat="server" Text="Search" OnClick="SearchButton_Click" />
+    <asp:Button ID="Button1" runat="server" Text="Search" CssClass="blue-button" OnClick="SearchButton_Click" />
+    <style>
+    .blue-button {
+        background-color: blue;
+        color: white;
+        padding: 5px 10px;
+        border: none;
+        cursor: pointer;
+    }
+</style>
+
+    <!-- Item List -->
+    <asp:GridView ID="ItemGridView" runat="server" AutoGenerateColumns="false">
+        <Columns>
+            <asp:BoundField DataField="ItemName" HeaderText="Item Name" />
+            <asp:BoundField DataField="Price" HeaderText="Price" />
+            <!-- Add other columns as needed -->
+        </Columns>
+    </asp:GridView>
+
+    <!-- Item Detail -->
+    <div>
+        <h2>Item Detail</h2>
+        <p><strong>Item Name:</strong> <asp:Label ID="ItemNameLabel" runat="server"></asp:Label></p>
+        <p><strong>Price:</strong> <asp:Label ID="PriceLabel" runat="server"></asp:Label></p>
+        <!-- Add other details as needed -->
+    </div>
+</asp:Content>
---
+++ Views/Browse.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PSDProject.Controller;
+using PSDProject.Model;
+
+namespace PSDProject.Views
+{
+    public partial class Views : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Load all items
+                LoadItems();
+            }
+        }
+
+        protected void SearchButton_Click(object sender, EventArgs e)
+        {
+            string searchQuery = SearchTextBox.Text;
+            // Perform search based on searchQuery
+            PerformSearch(searchQuery);
+        }
+
+        private void LoadItems()
+        {
+            // Get all items from the database
+            List<Item> items = ItemController.GetAllItems();
+
+            // Bind the items to the GridView
+            ItemGridView.DataSource = items;
+            ItemGridView.DataBind();
+        }
+
+        private void PerformSearch(string searchQuery)
+        {
+            // Perform search based on searchQuery
+            List<Item> searchResults = ItemController.SearchItems(searchQuery);
+
+            // Bind the search results to the GridView
+            ItemGridView.DataSource = searchResults;
+            ItemGridView.DataBind();
+        }
+
+        protected void ItemGridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the selected item index
+            int selectedIndex = ItemGridView.SelectedIndex;
+
+            // Retrieve the selected item from the GridView's DataKeys collection
+            int itemId = Convert.ToInt32(ItemGridView.DataKeys[selectedIndex].Value);
+
+            // Get the item details from the database
+            string itemIdString = itemId.ToString();
+            Item selectedItem = ItemController.GetOneItem(itemIdString);
+
+
+            // Display the item details
+            ItemNameLabel.Text = selectedItem.ItemName;
+            PriceLabel.Text = selectedItem.ItemPrice.ToString();
+            // Update other labels as needed
+        }
+    }
+}
